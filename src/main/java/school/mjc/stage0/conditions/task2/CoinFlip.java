a094d3e515e6c0877c645bccebfaed8e91e4b905
Станислав <stasxp3@gmail.com>
2023-11-04 23:38:45 +0100

    rty

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -1,7 +1,11 @@
 package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
-    public void throwCoin(int from1UpTo1000) {
-
+    public void throwCoin(int from1UpTo1000)
+    {
+        if(from1UpTo1000<500)
+            System.out.println("Eagle");
+        else
+            System.out.println("Tail");
     }
 }
